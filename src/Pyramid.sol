1ae6da89984be4d28c49450efafb5c4f5c4f448e
seonghwi lee <seonghwi.lee@dream-academy.io>
2023-03-12 15:45:53 +0900

    [UPDATE] Pyramid.sol

--- src/Pyramid.sol
+++ src/Pyramid.sol
@@ -1,17 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "forge-std/console.sol";
+
 contract Pyramid {
-    function run(uint size) pure public returns (string memory _out) {
-        _out = ""; // TODO
-        /* Example (size = 5):
-         *
-         *     "*\n"
-         *     "**\n"
-         *     "***\n"
-         *     "****\n"
-         *     "*****\n"
-         *
-         */
+    function run(uint n) public view returns (string memory _out) {
+        _out = "";
+
+        for (uint i = 0; i < n; i++) {
+            for (uint j = 0; j <= i; j++) _out = string.concat(_out, "*");
+            _out = string.concat(_out, "\n");
+        }
+
+        console.log("Result :\n%s", _out);
     }
 }
